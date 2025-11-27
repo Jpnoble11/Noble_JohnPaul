276932d1b64421fb5d408559f97bbe4135201e5c
JPNoble <123875257+Jpnoble11@users.noreply.github.com>
2025-11-27 13:01:06 +0800

    new

--- js/home.js
+++ js/home.js
@@ -1,15 +1,16 @@
 const sr = ScrollReveal({
-  distance: "40px",
-  duration: 2500,
+  distance: '40px',
+  duration: 2000,
   reset: true,
-});
+})
 
-sr.reveal("header", { delay: 600, origin: "top" });
-sr.reveal(".home-text span", { delay: 600, origin: "top" });
-sr.reveal(".home-text h1", { delay: 680, origin: "left" });
-sr.reveal(".home-text p", { delay: 750, origin: "right" });
-sr.reveal(".main-btn", { delay: 860, origin: "left" });
-sr.reveal(".home-img", { delay: 950, origin: "right" });
-
-sr.reveal(".social", { delay: 950, origin: "bottom" });
-sr.reveal("p", { delay: 950, origin: "bottom" });
+sr.reveal('header', { delay: 600, origin: 'top' })
+sr.reveal('.home-text span', { delay: 600, origin: 'top' })
+sr.reveal('.home-text h1', { delay: 680, origin: 'left' })
+sr.reveal('.home-text p', { delay: 750, origin: 'right' })
+sr.reveal('.main-btn', { delay: 860, origin: 'left' })
+sr.reveal('.home-img', { delay: 950, origin: 'right' })
+sr.reveal('.social', { delay: 950, origin: 'bottom' })
+sr.reveal('section', { delay: 600, origin: 'bottom' })
+sr.reveal('section h2', { delay: 700, origin: 'top' })
+sr.reveal('.about-img, .contact-img', { delay: 800, origin: 'left' })
--- js/index.js
+++ js/index.js
@@ -1,30 +1,73 @@
 function sendMail() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("message").value;
+  var name = document.getElementById('name').value
+  var email = document.getElementById('email').value
+  var message = document.getElementById('message').value
 
-  if (name === "" || email === "" || message === "") {
-    alert("Please fill in all fields.");
-    return;
+  if (name === '' || email === '' || message === '') {
+    alert('Please fill in all fields.')
+    return
   }
 
   var params = {
     name: name,
     email: email,
     message: message,
-  };
+  }
 
-  const serviceID = "service_r3cdxym";
-  const templateID = "template_ooqcm3z";
+  const serviceID = 'service_r3cdxym'
+  const templateID = 'template_ooqcm3z'
 
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("message").value = "";
-      console.log(res);
-      alert("Your message was sent successfully!");
+      document.getElementById('name').value = ''
+      document.getElementById('email').value = ''
+      document.getElementById('message').value = ''
+      console.log(res)
+      alert('Your message was sent successfully!')
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+
+  const canvas = document.getElementById('particles')
+  const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+  let particlesArray = []
+
+  class Particle {
+    constructor() {
+      this.x = Math.random() * canvas.width
+      this.y = Math.random() * canvas.height
+      this.size = Math.random() * 3 + 1
+      this.speedY = Math.random() * 1 - 0.5
+      this.color = '#00ffea'
+    }
+    update() {
+      this.y += this.speedY
+      if (this.y < 0) this.y = canvas.height
+    }
+    draw() {
+      ctx.fillStyle = this.color
+      ctx.fillRect(this.x, this.y, this.size, this.size)
+    }
+  }
+
+  function initParticles() {
+    particlesArray = []
+    for (let i = 0; i < 150; i++) {
+      particlesArray.push(new Particle())
+    }
+  }
+  function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    particlesArray.forEach((p) => {
+      p.update()
+      p.draw()
+    })
+    requestAnimationFrame(animateParticles)
+  }
+
+  initParticles()
+  animateParticles()
 }
